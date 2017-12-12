1b33efbdbfa86a386713cb13d10e910e772ddb15
omarsgalal <omarsgalal4@gmail.com>
2017-12-12 21:51:56 +0200

    login screen and connecting with api

--- App1.py
+++ App1.py
@@ -1,4 +1,7 @@
 import kivy
+import requests
+
+
 kivy.require('1.10.0')
 
 from kivy.app import App
@@ -11,6 +14,21 @@ class LoginScreen(BoxLayout):
 
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
+
+    def login(self):
+    	userName = self.ids.username_text.text
+    	password = self.ids.password_text.text
+    	if len(userName)>0 and len(password)>0:
+    		try:
+    			idUser = int(userName)
+    			payload = {'': [userName, password]}
+    			isLoggedin = requests.post('http://warehub-api.azurewebsites.net/login', data = payload)
+    			self.ids.username_text.text = str(isLoggedin.text)	
+    		except ValueError:
+    			message = 'invalid id'
+    	else:
+    		message = 'please enter your id and password'
+
 
 class MainMenu():
     def __init__(self, **kwargs):
