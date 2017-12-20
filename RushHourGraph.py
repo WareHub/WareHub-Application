fa7c6b7618d432a7497f3231f7400256cd551adf
AbdElRahman Kaseb <abdokaseb@users.noreply.github.com>
2017-12-21 00:06:44 +0200

    graphs for statstics
    graphs for statstics

---
+++ RushHourGraph.py
@@ -0,0 +1,44 @@
+from backend_kivyagg import FigureCanvasKivyAgg
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+import matplotlib.pyplot as plt
+import json
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.cbook as cbook
+import requests
+
+
+data = requests.get('http://warehub-api.azurewebsites.net/getrushhour')
+data = json.loads(data.text)
+date = []
+dss = []
+for s in data:
+    date.append(s[0][:2])
+    dss.append(s[1])
+
+fig, ax = plt.subplots()
+
+ax.plot( date,dss)
+ax.set_title('Rush Hours')
+
+ax.set_xlabel('Hour')
+ax.set_ylabel('Number of Demands')
+
+ax.set_ylim(0, max(dss)+5)
+
+ax.grid(True)
+
+fig.autofmt_xdate()
+
+
+class MyApp(App):
+
+    def build(self):
+        box = BoxLayout()
+        box.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+        return box
+
+MyApp().run()
